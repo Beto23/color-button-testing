1207b7814e83066d0b8e80662aa20f34211781ee
Jose Cordoba Lugo <jooseplugo@gmail.com>
2021-11-23 14:11:02 -0600

    add eslintrc.json and prettier settings - add plugin-jest-dom - add plugin-testing-library - remove eslintConfig from package.json - add vscode setttings and add standard config - add eslintcache and .vscode to .gitignore

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react'
 import App, { replaceCamelWhitespaces } from './App'
 
 test('button has the correct initial color', () => {
@@ -18,50 +18,58 @@ test('button has the correct initial color', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' })
 
   //expect the button text to be "Change to MediumVioletRed"
-  expect(colorButton.textContent).toBe('Change to Medium Violet Red')
+  expect(colorButton).toHaveTextContent('Change to Medium Violet Red')
 })
 
 test('initial conditions', () => {
   render(<App />)
   // chech that the button start enabled
-  const colorButton = screen.getByRole('button', { name: /change to Midnight Blue/i });
-  expect(colorButton).toBeEnabled();
+  const colorButton = screen.getByRole('button', {
+    name: /change to Midnight Blue/i,
+  })
+  expect(colorButton).toBeEnabled()
 
   //chech that the checkbox starts out unchecked
   const checkbox = screen.getByRole('checkbox')
 
-  expect(checkbox).not.toBeChecked();
+  expect(checkbox).not.toBeChecked()
 })
 
 test('Checkbox disables button on first click and enables to second click', () => {
   render(<App />)
-  const colorButton = screen.getByRole('button', { name: /change to Midnight Blue/i });
-  const checkbox = screen.getByRole('checkbox', { name: /disabled button/i });
-
-  fireEvent.click(checkbox);
-  expect(colorButton).toBeDisabled();
+  const colorButton = screen.getByRole('button', {
+    name: /change to Midnight Blue/i,
+  })
+  const checkbox = screen.getByRole('checkbox', { name: /disabled button/i })
 
   fireEvent.click(checkbox)
-  expect(colorButton).toBeEnabled();
+  expect(colorButton).toBeDisabled()
+
+  fireEvent.click(checkbox)
+  expect(colorButton).toBeEnabled()
 })
 
 test('Disabled button has gray background and revert to MediumVioletRed', () => {
-  render(<App />);
-  const colorButton = screen.getByRole('button', { name: /change to Midnight Blue/i });
-  const checkbox = screen.getByRole('checkbox', { name: /disabled button/i });
+  render(<App />)
+  const colorButton = screen.getByRole('button', {
+    name: /change to Midnight Blue/i,
+  })
+  const checkbox = screen.getByRole('checkbox', { name: /disabled button/i })
 
-  fireEvent.click(checkbox);
-  expect(colorButton).toBeDisabled();
-  expect(colorButton).toHaveStyle({backgroundColor: 'gray'});
+  fireEvent.click(checkbox)
+  expect(colorButton).toBeDisabled()
+  expect(colorButton).toHaveStyle({ backgroundColor: 'gray' })
 
-  fireEvent.click(checkbox);
-  expect(colorButton).toBeEnabled();
-  expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
+  fireEvent.click(checkbox)
+  expect(colorButton).toBeEnabled()
+  expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' })
 })
 
 test('Clicked disabled button has gray background and revert to MidnightBlue', () => {
   render(<App />)
-  const colorButton = screen.getByRole('button', { name: /change to Midnight Blue/i })
+  const colorButton = screen.getByRole('button', {
+    name: /change to Midnight Blue/i,
+  })
   const checkbox = screen.getByRole('checkbox', { name: /disabled button/i })
 
   fireEvent.click(colorButton)
@@ -72,16 +80,15 @@ test('Clicked disabled button has gray background and revert to MidnightBlue', (
   fireEvent.click(checkbox)
   expect(colorButton).toBeEnabled()
   expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' })
-});
+})
 
 // unit function test
 describe('spaces before camel-case cappital letters', () => {
   it('Works for no inner cappital letters', () => {
-    expect(replaceCamelWhitespaces('Red')).toBe('Red');
+    expect(replaceCamelWhitespaces('Red')).toBe('Red')
   })
   it('Works for one inner cappital letters', () => {
     expect(replaceCamelWhitespaces('MidnighBlue')).toBe('Midnigh Blue')
-
   })
   it('Works for multiple inner cappital letters', () => {
     expect(replaceCamelWhitespaces('MediumVioletRed')).toBe('Medium Violet Red')
